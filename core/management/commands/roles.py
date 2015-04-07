eaa2d3d14cb8483620133dee8e46ca36e0feaa81
Daniel Padula <daniel.padula@gmail.com>
2015-04-07 12:37:04 -0300

    Fix name

--- core/management/commands/roles.py
+++ core/management/commands/roles.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
             queryset = queryset.filter(code__in=options['codes'].split(','))
 
         for role in queryset:
-            self.stdout.write('Role {}:\n\tCode: {}\n\tNum of users using it: {}\n\tGrants:'.format(
+            self.stdout.write('Role {}:\n\tCode: {}\n\tNum of users using it: {}\n\tPrivileges:'.format(
                 role.name,
                 role.code,
                 User.objects.filter(roles__id=role.id).count()
