137406b747777a2d6a4647b7d1ffb8cb312fa5e4
avdata99 <andres@data99.com.ar>
2015-04-08 15:41:39 -0300

    agregar estadisticas basicas al contexto de los templates

--- workspace/middlewares/ioc.py
+++ workspace/middlewares/ioc.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-
+from core.models import Dataset, DataStream, Visualization, Dashboard
+from core.cache import Cache
 """Inversion Of Control """
+
 
 class DependenciesInjector(object):
 
@@ -9,7 +11,7 @@ class DependenciesInjector(object):
         """
         Checks user, role and account
         """
-        if request.user == None:
+        if not request.user:
             request.preferences = {}
             return None
 
@@ -21,6 +23,36 @@ class DependenciesInjector(object):
         if not bucket_name:
             bucket_name = settings.AWS_BUCKET_NAME
 
+        self.read_stats(request)
         request.bucket_name = bucket_name
 
         return None
+
+    def read_stats(self, request):
+        user_id = request.auth_manager.id
+        c = Cache(db=0)
+
+        my_total_datasets = c.get('my_total_datasets_' + str(user_id))
+        if not my_total_datasets:
+            my_total_datasets =  Dataset.objects.filter(user=user_id).count()
+            c.set('my_total_datasets_' + str(user_id), my_total_datasets, settings.REDIS_STATS_TTL)
+        request.my_total_datasets = my_total_datasets
+
+        my_total_datastreams = c.get('my_total_datastreams_' + str(user_id))
+        if not my_total_datastreams:
+            my_total_datastreams = DataStream.objects.filter(user=user_id).count()
+            c.set('my_total_datastreams_' + str(user_id), my_total_datastreams, settings.REDIS_STATS_TTL)
+        request.my_total_datastreams = my_total_datastreams
+
+        my_total_dashboards = c.get('my_total_dashboards_' + str(user_id))
+        if not my_total_dashboards:
+            my_total_dashboards = Dashboard.objects.filter(user=user_id).count()
+            c.set('my_total_dashboards_' + str(user_id), my_total_dashboards, settings.REDIS_STATS_TTL)
+        request.my_total_dashboards = my_total_dashboards
+
+        my_total_visualizations = c.get('my_total_visualizations_' + str(user_id))
+        if not my_total_visualizations:
+            my_total_visualizations = Visualization.objects.filter(user=user_id).count()
+            c.set('my_total_visualizations_' + str(user_id), my_total_visualizations, settings.REDIS_STATS_TTL)
+
+        request.my_total_visualizations = my_total_visualizations
