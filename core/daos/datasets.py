15e5232f404de9fefd87fc336f5562ee4b9b2da8
avdata99 <andres@data99.com.ar>
2015-04-14 15:15:14 -0300

    cambios en DAOs

--- core/daos/datasets.py
+++ core/daos/datasets.py
@@ -1,21 +1,76 @@
 # -*- coding: utf-8 -*-
 
 import time
-from core.models import DatasetI18n
+from core.models import DatasetI18n, Dataset, DatasetRevision, Category
 from core.exceptions import SearchIndexNotFoundException
 from core.lib.searchify import SearchifyIndex
 from core import settings
 from core.daos.resource import AbstractDatasetDBDAO
+from core.builders.datasets import DatasetImplBuilderWrapper
 
 
 class DatasetDBDAO(AbstractDatasetDBDAO):
     """ class for manage access to datasets' database tables """
 
+    def get(self, language, dataset_id=None, dataset_revision_id=None):
+        pass
+        
     def query(self, account_id=None, language=None, page=0, itemsxpage=settings.PAGINATION_RESULTS_PER_PAGE,
           sort_by='-id', filters_dict=None, filter_name=None, exclude=None):
+              
+        pass
 
-        return super(DatasetDBDAO, self).query(account_id, language, page, itemsxpage, sort_by, filters_dict, filter_name, exclude)
+    def query_childs(self, dataset_id, language):
+        """ Devuelve la jerarquia completa para medir el impacto """
+        pass
 
+    def create(self, dataset=None, user=None, collect_type='', impl_details=None, **fields):
+        """create a new dataset if needed and a dataset_revision"""
+
+        if dataset is None:
+            # Create a new dataset (TITLE is for automatic GUID creation)
+            dataset = Dataset.objects.create(user=user, type=collect_type, title=fields['title'])
+
+        # meta_text = '{"field_values": [{"cust-dataid": "dataid-%s"}]}' % dataset.id
+        if not fields.get('language', None):
+            fields['language'] = user.language
+
+        dataset_revision = DatasetRevision.objects.create(dataset=dataset,
+                user_id=user.id, status=fields['status'],
+                category=Category.objects.get(id=fields['category']), filename=fields['file_name'],
+                end_point=fields['end_point'], impl_type=fields['impl_type'],
+                impl_details=impl_details, size=fields['file_size'],
+                license_url=fields['license_url'], spatial=fields['spatial'],
+                frequency=fields['frequency'], mbox=fields['mbox'])
+
+        DatasetI18n.objects.create(dataset_revision=dataset_revision,
+            language=fields['language'], title=fields['title'],
+            description=fields['description'], notes=fields['notes'])
+
+        dataset_revision.add_tags(fields['tags'])
+        dataset_revision.add_sources(fields['sources'])
+
+        return dataset, dataset_revision
+
+    def update(self, dataset_revision, changed_fields, **fields):
+        builder = DatasetImplBuilderWrapper(changed_fields=changed_fields, **fields).builder
+
+        # TODO: Fix that
+        #if builder.has_changed(changed_fields):
+        #    # Build impl_details if necessary
+        fields['impl_details'] = builder.build()
+
+        changed_fields.append('impl_details')
+
+        dataset_revision.update(changed_fields, **fields)
+
+        DatasetI18n.objects.get(dataset_revision=dataset_revision, language=fields['language']).update(changed_fields, **fields)
+
+        dataset_revision.add_tags(fields['tags'])
+        dataset_revision.add_sources(fields['sources'])
+
+        return dataset_revision
+        
 class DatasetSearchDAOFactory():
     """ select Search engine"""
     
--- core/daos/datastreams.py
+++ core/daos/datastreams.py
@@ -2,18 +2,62 @@
 from core.exceptions import SearchIndexNotFoundException
 from core import settings
 from core.daos.resource import AbstractDataStreamDBDAO
-from core.models import DatastreamI18n
+from core.models import DatastreamI18n, DataStream, DataStreamRevision
 
 
 class DataStreamDBDAO(AbstractDataStreamDBDAO):
     """ class for manage access to datastreams' database tables """
 
+    def create(self, datastream=None, user=None, **fields):
+        """create a new datastream if needed and a datastream_revision"""
+
+        if datastream is None:
+            # Create a new datastream (TITLE is for automatic GUID creation)
+            datastream = DataStream.objects.create(user=user, title=fields['title'])
+
+        # meta_text = 
+
+        datastream_revision = DataStreamRevision.objects.create(datastream=datastream
+                                                                , user=user
+                                                                , dataset=fields['dataset']
+                                                                , status=fields['status']
+                                                                , category=fields['category']
+                                                                , data_source=fields['data_source']
+                                                                , select_statement=fields['select_statement'])
+
+        DatastreamI18n.objects.create(datastream_revision=datastream_revision,
+            language=fields['language'], title=fields['title'],
+            description=fields['description'], notes=fields['notes'])
+
+        datastream_revision.add_tags(fields['tags'])
+        datastream_revision.add_sources(fields['sources'])
+        datastream_revision.add_parameters(fields['parameters'])
+
+        return datastream, datastream_revision
+
+    def update(self, datastream_revision, changed_fields, **fields):
+
+        datastream_revision.update(changed_fields, **fields)
+
+        DatastreamI18n.objects.get(datastream_revision=datastream_revision, language=fields['language']).update(changed_fields, **fields)
+
+        datastream_revision.add_tags(fields['tags'])
+        datastream_revision.add_sources(fields['sources'])
+
+        return datastream_revision
+
+    def get(self, language, datastream_id=None, datastream_revision_id=None):
+        pass
+        
     def query(self, account_id=None, language=None, page=0, itemsxpage=settings.PAGINATION_RESULTS_PER_PAGE,
               sort_by='-id', filters_dict=None, filter_name=None, exclude=None):
 
-        return super(DataStreamDBDAO, self).query(account_id, language, page, itemsxpage,
-                                                  sort_by, filters_dict, filter_name, exclude)
+        pass
 
+    def query_childs(self, dataset_id, language):
+        """ Devuelve la jerarquia completa para medir el impacto """
+        pass
+        
 class DatastreamSearchDAOFactory():
     """ select Search engine"""
     
