3400426028793aba090f0c921d6d2ab219331fdb
Juan Pablo <juan33650@outlook.com>
2015-05-04 18:09:51 -0300

    [Workspace] Implementación de overlay 'Editar' en ficha de Vista.

--- workspace/js/viewDataStream/datastreamEditItemModel.js
+++ workspace/js/viewDataStream/datastreamEditItemModel.js
@@ -63,7 +63,7 @@ var DatastreamEditItemModel = Backbone.Epoxy.Model.extend({
         data.title = $.trim( this.get('title') );
         data.description = $.trim( this.get('description') );
         data.category = $('#id_category option:selected').val();
-        data.status = $('#id_status option:selected').val();
+        data.status = $('#id_datastream_status option:selected').val();
         data.datastream_revision_id = this.get('datastream_revision_id');
 
         // Prepare Sources for Data
