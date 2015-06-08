6c58c44a47b34ad1a84ae6fa70eb154cceca21fb
Daniel Padula <daniel.padula@gmail.com>
2015-06-08 19:46:03 -0300

    Limpieza

--- core/lib/mail/mail.py
+++ core/lib/mail/mail.py
@@ -20,17 +20,8 @@ class MailService():
         """ Suscribir a la lista de usuarios del sistema"""
         pass
 
-# TODO: Verificar este codigo para hacer el import dinamicamente
 mail_service = None
 class_name = settings.EMAIL_SERVICE.split('.')[-1]
 module_name = settings.EMAIL_SERVICE.split('.{}'.format(class_name))[0]
 mail_service = class_for_name(module_name=module_name, class_name=class_name)
 
-#TODO: Cuando ande el dinamico borrar esto
-#mail_service = None
-#if settings.EMAIL_SERVICE == 'core.lib.mail.mailchimp_backend.MailchimpMailService':
-#    from core.lib.mail.mailchimp import MailchimpMailService
-#    mail_service = MailchimpMailService()
-#elif settings.EMAIL_SERVICE == 'core.lib.mail.django_backend.DjangoMailService':
-#    from core.lib.mail.django_backend import DjangoMailService
-#    mail_service = DjangoMailService()
