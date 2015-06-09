4b610348271f99bcebc3e2116dc1e3284cad1879
Daniel Padula <daniel.padula@gmail.com>
2015-06-09 18:37:23 -0300

    Fix del comando de inicio de indextank

--- salt/roots/salt/core/indextank/indextank-engine.sh
+++ salt/roots/salt/core/indextank/indextank-engine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /usr/share/indextank-engine
-exec java -cp target/indextank-engine-1.0.0-jar-with-dependencies.jar com.flaptor.indextank.api.Launcherc --load-state true < /dev/null > /tmp/indextank-engine.log 2>&1 &
+exec java -cp target/indextank-engine-1.0.0-jar-with-dependencies.jar com.flaptor.indextank.api.Launcher --load-state true < /dev/null > /tmp/indextank-engine.log 2>&1 &
 
