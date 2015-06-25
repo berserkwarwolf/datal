926d980f6fcd475120172ac8767d243188af159e
Javier Pájaro <javier.pajaro@junar.com>
2015-06-25 11:53:36 -0300

    Se agrega opcion a status_choices para que no se rompa con approve

--- core/choices.py
+++ core/choices.py
@@ -11,6 +11,7 @@ class StatusChoices():
     PENDING_REVIEW = 1
     UNDER_REVIEW = 2
     PUBLISHED = 3
+    UNPUBLISHED = 4
     REJECTED = 5
     APPROVED = 6
 
@@ -34,6 +35,7 @@ STATUS_CHOICES = (
     ,(StatusChoices.PENDING_REVIEW,  ugettext_lazy('MODEL_STATUS_PENDING_REVIEW'))
     ,(StatusChoices.UNDER_REVIEW,  ugettext_lazy('MODEL_STATUS_UNDER_REVIEW'))
     ,(StatusChoices.PUBLISHED,  ugettext_lazy('MODEL_STATUS_PUBLISHED'))
+    ,(StatusChoices.UNPUBLISHED,  '')
     ,(StatusChoices.REJECTED,  ugettext_lazy('MODEL_STATUS_REJECTED'))
     ,(StatusChoices.APPROVED,  ugettext_lazy('MODEL_STATUS_APPROVED'))
 )
