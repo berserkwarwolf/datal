c85008b545a31626788e35f828862cb27ba97148
avdata99 <andres@data99.com.ar>
2015-07-03 18:47:03 -0300

    add 404 for dataviews

--- workspace/manageDataviews/views.py
+++ workspace/manageDataviews/views.py
@@ -15,7 +15,7 @@ from workspace.templates import *
 from workspace.daos.datastreams import DataStreamDBDAO
 from core.lifecycle.datastreams import DatastreamLifeCycleManager
 from workspace.exceptions import LifeCycleException
-from core.models import DatasetRevision, Account, CategoryI18n
+from core.models import DatasetRevision, Account, CategoryI18n, DataStreamRevision
 from api.http import JSONHttpResponse
 from core import engine
 
@@ -24,17 +24,20 @@ logger = logging.getLogger(__name__)
 
 @login_required
 @require_GET
-def view(request, revision_id):
+def action_view(request, revision_id):
+    logger.error('loadin DV %s' % str(revision_id))
+    
+    language = request.auth_manager.language
+    try:
+        datastream = DataStreamDBDAO().get(language, datastream_revision_id=revision_id)
+    except DataStreamRevision.DoesNotExist:
+        raise DataStreamNotFoundException
+
     account_id = request.auth_manager.account_id
     credentials = request.auth_manager
-    language = request.auth_manager.language
-    categories = CategoryI18n.objects.filter(language=language, category__account=account_id).values(
-        'category__id',
-        'name'
-    )
+    categories = CategoryI18n.objects.filter(language=language, category__account=account_id).values('category__id','name')
     status_options = credentials.get_allowed_actions()
-    datastream = DataStreamDBDAO().get(language, datastream_revision_id=revision_id)
-
+    
     return render_to_response('viewDataStream/index.html', locals())
 
 @login_required
