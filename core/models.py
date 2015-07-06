92a27547c67c030f0f8ba5c1082709def6c1589d
Daniel Padula <daniel.padula@gmail.com>
2015-07-06 17:58:27 -0300

    Metodo para eliminar datastreams relacionados a dataset

--- core/models.py
+++ core/models.py
@@ -297,12 +297,24 @@ class DataStreamRevision(models.Model):
     objects = managers.DataStreamRevisionManager()
 
     class Meta:
-        db_table        = 'ao_datastream_revisions'
-        ordering        = ['-id']
-        get_latest_by   = 'created_at'
+        db_table = 'ao_datastream_revisions'
+        ordering = ['-id']
+        get_latest_by = 'created_at'
 
     def __unicode__(self):
         return unicode(self.id)
+
+    @staticmethod
+    def remove_related_to_dataset(dataset):
+        """
+        Elimino las revisiones de DataStream asociados a un dataset en particular y su DataStream
+        """
+        datastream_ids = set()
+        datastream_revisions = DataStreamRevision.objects.filter(dataset=dataset)
+        for datastreamrevision in datastream_revisions:
+            datastream_ids.add(datastreamrevision.id)
+            datastreamrevision.delete()
+        DataStream.objects.filter(pk__in=datastream_ids).delete()
 
     def update(self, changed_fields, **fields):
         if changed_fields:
