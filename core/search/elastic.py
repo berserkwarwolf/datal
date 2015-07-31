96c8078e7142d7d30b36fcf5c7e1bba97ccbfbb6
Gaston Avila <avila.gas@gmail.com>
2015-07-31 12:02:17 -0300

    pep8 in the honor of Daniel Padula

--- core/search/elastic.py
+++ core/search/elastic.py
@@ -37,12 +37,12 @@ class ElasticsearchFinder(Finder):
             start = (page - 1) * end
 
         if self.sort == "" or self.sort == "1":
-            self.sort=self.order_by
+            self.sort = self.order_by
 
         # Tengo que saber para qué se usa esto
         self.meta_data = kwargs.get('meta_data', {})
 
-        ### CATEGORY FILTERS
+        # CATEGORY FILTERS
         category_filters = kwargs.get('category_filters', None)
         if category_filters is None:
             category_filters = {}
@@ -51,19 +51,23 @@ class ElasticsearchFinder(Finder):
 
         scoring = kwargs.get('scoring', 1)
 
-        query=self.__build_query()
-	self.logger.info("Query arguments: %s (%s)" % (query,self.sort))
-        results = self.index.es.search(index=settings.SEARCH_INDEX['index'],body=query, from_=start, size=end, sort=self.sort)
+        query = self.__build_query()
+        self.logger.info("Query arguments: %s (%s)" % (query, self.sort))
+        results = self.index.es.search(index=settings.SEARCH_INDEX['index'],
+                                       body=query,
+                                       from_=start,
+                                       size=end,
+                                       sort=self.sort)
 
         # re arma los documentos incluyendo a la categorias
-        docs=[]
+        docs = []
         for i in results['hits']['hits']:
-            i['_source']['fields']['category']=i['_source']['categories']
-            i['_source']['fields']['docid']=i['_source']['docid']
+            i['_source']['fields']['category'] = i['_source']['categories']
+            i['_source']['fields']['docid'] = i['_source']['docid']
             docs.append(i['_source']['fields'])
 
-        search_time     = float(results['took'])/1000
-        facets          = results['facets']['type']['terms']
+        search_time = float(results['took'])/1000
+        facets = results['facets']['type']['terms']
 
         results = []
         for doc in docs:
@@ -75,23 +79,45 @@ class ElasticsearchFinder(Finder):
         return results, search_time, facets
 
     def __build_query(self):
-        self.logger.info("El query es: %s" % self.query) 
+        self.logger.info("El query es: %s" % self.query)
 
         # decide que conjunto de recursos va a filtrar
         if self.resource == "all":
-            self.resource=["ds","dt","db","chart","vt"]
+            self.resource = ["ds", "dt", "db", "chart", "vt"]
 
         # previene un error al pasarle un string y no un LIST
         if type(self.resource) == type(str()):
-            self.resource=[self.resource]
+            self.resource = [self.resource]
 
-        query={"query": {
-            "filtered":{
-                "query" : { "query_string" : {"query": "*%s*" % self.query, "fields":["title","text"] }},
-                "filter": {"bool" : { "must" :[
-                    { "terms" : {"type" : self.resource} },
-                    { "term" : {"account_id" : self.account_id}}
-                    ]}}}},
-            "facets" : { "type" : { "terms" : {"field" : "categories.name"} } } }
+        query = {
+            "query": {
+                "filtered": {
+                    "query": {
+                        "query_string": {
+                            "query": "*%s*" % self.query,
+                            "fields": ["title", "text"]
+                        }
+                    },
+                    "filter": {
+                        "bool": {
+                            "must": [
+                                {
+                                    "term": {"account_id": self.account_id}
+                                },
+                                {
+                                    "terms": {"type": self.resource}
+                                }
+                            ]
+                        }
+                    }
+                }
+            },
+            "facets": {
+                "type": {
+                    "terms": {
+                        "field": "categories.name"
+                    }
+                }
+            }
+        }
         return query
-
