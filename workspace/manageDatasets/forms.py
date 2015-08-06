1bb8100ad1bcaf22236b8d969c70509ab1344a0a
Daniel Padula <daniel.padula@gmail.com>
2015-08-06 10:49:57 -0300

    No andaba la validacion de rar en windows

--- workspace/manageDatasets/forms.py
+++ workspace/manageDatasets/forms.py
@@ -344,7 +344,7 @@ class FileForm(DatasetForm):
     def clean(self):
         if 'file_data' in self.cleaned_data.keys():
             if self.cleaned_data['file_data'].content_type in ['image/jpeg', 'application/zip',
-                                                               'application/x-rar']:
+                                                               'application/x-rar', 'application/octet-stream']:
                  raise FileTypeNotValidException()
         return self.cleaned_data
 
