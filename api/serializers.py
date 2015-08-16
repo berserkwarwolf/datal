4af7dad21158e5cf5ad64e1e528ac21106c3dbe0
Matias Saguir <mativs@gmail.com>
2015-08-16 11:37:52 -0300

    Datastreams rows working

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
 from core.models import DataStream
 from core.docs import DS
+from api.helpers import add_domain_to_datastream_link
 
 
 class DataStreamSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataStream
-    
+        fields = ()
+
     def to_representation(self, obj):
-        language = 'en' # TODO: need to get from user
-        try:
-            datastreamrevision_id =  obj.datastreamrevision_set.latest().id #DataStreamRevision.objects.get_last_published_id(self.id)
-            doc = DS(datastreamrevision_id, language)
-        except Exception:
-            raise
-
-
+        language = 'en'
+        if 'request' in self.context:
+            request = self.context['request']
+            passticket = request.GET.get('passticket', None)
+            if passticket:
+                language = Account.objects.get(pk=request.account_id).get_preference('account.language')
+        
+        datastreamrevision_id =  obj.datastreamrevision_set.latest().id #DataStreamRevision.objects.get_last_published_id(self.id)
+        doc = DS(datastreamrevision_id, language)
+        
         answer = super(DataStreamSerializer, self).to_representation(obj)
 
         answer['id']=doc.guid
@@ -26,6 +30,7 @@ class DataStreamSerializer(serializers.ModelSerializer):
         answer['created_at']=str(doc.created_at)
         answer['source']=doc.filename
         answer['link']=doc.permalink()
+        add_domain_to_datastream_link(answer)
 
         if doc.parameters:
             parameters = []
--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from api.http import HttpResponseNotFound, HttpResponseServerError
-from core.models import DataStream, DataStreamRevision
+from rest_framework.response import Response
+from core.models import DataStream, DataStreamRevision, DataStreamParameter
 from rest_framework import viewsets
 from api.serializers import DataStreamSerializer
+from rest_framework.decorators import detail_route, list_route
+from core.datastream_manager import forms
+from core.engine import invoke
+from core.helpers import RequestProcessor
+from rest_framework import status
+import logging
+import json
+from core import engine
 
 def action404(p_request):
     return HttpResponseNotFound('{"error": 404, "message": "API Not Found"}')
@@ -13,3 +22,20 @@ class DataStreamViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DataStream.objects.all()
     serializer_class = DataStreamSerializer
     lookup_field = 'guid'
+
+    @detail_route(methods=['get'])
+    def rows(self, request, guid=None, pk=None, format='json'):
+        datastream = self.get_object()
+        mutable_get = request.GET.copy()
+        mutable_get['datastream_revision_id'] = datastream.last_published_revision_id
+        form = forms.RequestForm(mutable_get)
+        if form.is_valid():
+            query = RequestProcessor(request).get_arguments_no_validation()
+            query['pId'] = form.cleaned_data.get('datastream_revision_id')
+            limit = form.cleaned_data.get('limit')
+            query['pLimit'] = limit or 50
+
+            ivk = invoke(query)
+            if ivk:
+                return Response(json.loads(ivk[0]))
+        return Response('{"Error":"No invoke"}', status=status.HTTP_400_BAD_REQUEST)
