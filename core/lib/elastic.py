3354862f0de471dedf016acf058a176246ec0793
Ignacio Feijoo <ignacionf@gmail.com>
2015-08-22 02:24:19 -0300

    mejoras en el update

--- core/lib/elastic.py
+++ core/lib/elastic.py
@@ -251,7 +251,7 @@ class ElasticsearchIndex():
         """ update by id"""
 
         try:
-            return self.es.update(index=settings.SEARCH_INDEX['index'], id=document['docid'], doc_type=document['type'], body={"doc":document})
+            return self.es.update(index=settings.SEARCH_INDEX['index'], id=document['docid'], doc_type=document['type'], body=document)
         except RequestError,e:
             raise RequestError(e)
         except NotFoundError,e:
