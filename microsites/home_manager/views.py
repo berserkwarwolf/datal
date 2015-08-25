c420086f1a9f08ed6b25d0366fcd0a33cad96552
Daniel Padula <daniel.padula@gmail.com>
2015-08-25 14:57:58 -0300

    Limpiando codigo deprecado

--- microsites/home_manager/views.py
+++ microsites/home_manager/views.py
@@ -1,5 +1,4 @@
 from core.shortcuts import render_to_response
-from core.communitymanagers import *
 from core.http import get_domain
 from core.daos.datastreams import DataStreamDBDAO
 from core.daos.visualizations import VisualizationDBDAO
@@ -7,7 +6,6 @@ from core.daos.visualizations import VisualizationDBDAO
 
 def action_sitemap(request):
     import datetime
-    logger = logging.getLogger(__name__)
 
     language = request.auth_manager.language
     account = request.account
