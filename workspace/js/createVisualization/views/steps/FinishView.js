cb5799b5314e4978090961eeb2342c80513dfa29
Pablo H. Paladino <paladinop@gmail.com>
2015-08-26 12:29:31 -0300

    mejoras en proceso de metadata y sve

--- workspace/js/createVisualization/views/steps/FinishView.js
+++ workspace/js/createVisualization/views/steps/FinishView.js
@@ -14,7 +14,8 @@ var FinishView = StepViewSPA.extend({
 	},
 
 	onFinishButtonClicked: function(){		
-		var data = this.model.getSettings();
+		var data = this.model.getFormData();
+		console.log(data);
 		$.ajax({
 			url: '/visualizations/create?datastream_revision_id='
 				+ this.model.get('datastream_revision_id'),
