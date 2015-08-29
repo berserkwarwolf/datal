9c9d7c628fe72e3a61831bc55161bc7880af41cd
Matias Saguir <mativs@gmail.com>
2015-08-29 18:46:46 -0300

    Improve error handling to catch unexpeted error

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,15 +1,31 @@
 from rest_framework.views import exception_handler
+from rest_framework.response import Response
+from rest_framework.compat import set_rollback
+from rest_framework import status
+import logging
+import sys, traceback
 
-def datal_exception_handler(exc, context):
+def datal_exception_handler(exception, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
-    response = exception_handler(exc, context)
+    response = exception_handler(exception, context)
 
     # Now add the HTTP status code to the response.
     if response is not None:
         response.data['status'] = response.status_code
         response.data['description'] = response.data.pop('detail')
-        response.data['error'] = str(exc.__class__.__name__)
+        response.data['error'] = str(exception.__class__.__name__)
         response.data['type'] = 'api-error'
+    else:
+        logger = logging.getLogger(__name__)
+        trace = '\n'.join(traceback.format_exception(*(sys.exc_info())))
+        logger.error('[UnexpectedCatchError] %s. %s %s' % (
+                str(exception), repr(exception), trace))
+        set_rollback()
+        response = Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        response.data['status'] = response.status_code
+        response.data['description'] = str(exception)
+        response.data['error'] = str(exception.__class__.__name__)
+        response.data['type'] = 'unexpected-error'
 
     return response
