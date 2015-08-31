9fd105569d2aaf7b151a345fa47fde9df5bcdc7c
Javier Pájaro <javier.pajaro@junar.com>
2015-08-31 17:05:14 -0300

    Agregando el preview charts

--- microsites/chart_manager/forms.py
+++ microsites/chart_manager/forms.py
@@ -5,4 +5,14 @@ class RequestForm(forms.Form):
     limit = forms.IntegerField(required=False)
     page = forms.IntegerField(required=False)
     bounds = forms.CharField(required=False)
-    zoom = forms.IntegerField(required=False)     
+    zoom = forms.IntegerField(required=False)
+
+class PreviewForm(RequestForm):
+    null_action = forms.CharField(required=True)
+    null_preset = forms.CharField(required=True)
+    data = forms.CharField(required=True)
+    labels = forms.CharField(required=False)
+    headers = forms.CharField(required=False)
+    lat = forms.CharField(required=False)
+    long = forms.CharField(required=False)
+    traces = forms.CharField(required=False)
