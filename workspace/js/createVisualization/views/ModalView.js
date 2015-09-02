399da1ed0ed6b798d9412b9ceaca49c8a65f4241
Juan Pablo <juan33650@outlook.com>
2015-09-02 17:50:14 -0300

    Scrollbar oculta al abrir modal de selección.

--- workspace/js/createVisualization/views/ModalView.js
+++ workspace/js/createVisualization/views/ModalView.js
@@ -1,12 +1,14 @@
 var ModalView = Backbone.View.extend({
 
 	open: function(){
+		$('body').css('overflow', 'hidden');
 		$('.process-manager-modal').addClass('hidden');
 		this.$el.removeClass('hidden');
 		this.trigger('open');
 	},
 
 	close: function(){
+		$('body').css('overflow', 'auto');
 		this.$el.addClass('hidden');
 		this.trigger('close');
 	},
