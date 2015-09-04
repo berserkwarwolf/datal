a86bfe7b2ee02f11e6571c077b3a98a647566daa
Daniel Padula <daniel.padula@gmail.com>
2015-09-03 22:33:39 -0300

    Fixes after merge

--- core/urls.py
+++ core/urls.py
@@ -18,14 +18,14 @@ urlpatterns = patterns('',
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 
-    (r'^accounts/', include('workspace.accounts.urls')),
+    (r'^accounts/', include('workspace.manageMyAccount.urls')),
     (r'^dataset/', include('workspace.dataset_manager.urls')),
     (r'^review/', include('workspace.review_manager.urls')),
 
     (r'^js_core/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'core', 'js')}),
 
     # please leave me always as the last url pattern
-    url(r'^(?P<admin_url>[A-Za-z0-9\-]+)/$', 'workspace.accounts.views.signin', name='accounts.account_signin'),
+    url(r'^(?P<admin_url>[A-Za-z0-9\-]+)/$', 'workspace.manageMyAccount.views.signin', name='accounts.account_signin'),
 )
 
 handler404 = 'core.views.action404'
