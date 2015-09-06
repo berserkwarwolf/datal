e77e2990f4a03374209d6049c63a657a16e28ed5
Daniel Padula <daniel.padula@gmail.com>
2015-09-06 18:47:49 -0300

    Limpieza 5

--- microsites/share_manager/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import *
-from microsites.share_manager.views import *
-
-urlpatterns = patterns('',
-    url(r'^linkedin', action_linkedin, name='share_manager.action_linkedin')
-)
