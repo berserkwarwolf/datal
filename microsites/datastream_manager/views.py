89ece79f8077976c92ede5842f2b37001ff8fc1f
Daniel Padula <daniel.padula@gmail.com>
2015-09-06 19:42:45 -0300

    Limpieza bla

--- microsites/datastream_manager/views.py
+++ microsites/datastream_manager/views.py
@@ -10,66 +10,10 @@ from django.utils.translation import ugettext_lazy
 from django.template import loader, Context
 
 from core.choices import ChannelTypes
-from core.daos.datastreams import DataStreamDBDAO
 from core.helpers import RequestProcessor
-from core.http import get_domain_with_protocol
-from core.models import DataStreamRevision, DataStream, Account, Visualization
+from core.models import DataStreamRevision, DataStream, Visualization
 from core.shortcuts import render_to_response
 from core.daos.datastreams import DatastreamHitsDAO, DataStreamDBDAO
-from core.utils import set_dataset_impl_type_nice
-
-
-def action_view(request, id, slug):
-    DOC_API_URL = settings.DOC_API_URL
-    logger = logging.getLogger(__name__)
-
-    try:
-        account = request.account
-        is_free = False
-    except AttributeError:
-        try:
-            account_id = DataStream.objects.values('user__account_id').get(pk=id)['user__account_id']
-            account = Account.objects.get(pk=account_id)
-            is_free = True
-        except (DataStream.DoesNotExist, Account.DoesNotExist), e:
-            logger.debug('Datstream or account doesn\'t exists [%s|%s]=%s' % (str(id), str(account_id), repr(e)))
-            raise Http404
-
-    preferences = request.preferences
-    if not is_free:
-        base_uri = 'http://' + preferences['account_domain']
-    else:
-        base_uri = get_domain_with_protocol('microsites')
-
-    datastreamrevision_id = DataStreamRevision.objects.get_last_published_id(id)
-
-    datastream = DataStreamDBDAO().get(preferences['account_language'], datastream_revision_id=datastreamrevision_id)
-    impl_type_nice = set_dataset_impl_type_nice(datastream['impl_type']).replace('/',' ')
-
-    """ #TODO this must be at middleware
-    # verify if this account is the owner of this viz
-    dats = DataStream.objects.get(pk=id)
-    if account.id != dats.user.account.id:
-        logger.debug('Can\'t show. Not owner [%s|%s]=%s' % (id, str(account.id), str(dats.user.account.id), "Not owner"))
-        raise Http404
-    """
-    url_query = urllib.urlencode(RequestProcessor(request).get_arguments(datastream['parameters']))
-
-    can_download = preferences['account_dataset_download'] == 'on' or preferences['account_dataset_download'] or preferences['account_dataset_download'] == 'True'
-    can_export = True
-    can_share = False
-
-    DatastreamHitsDAO(datastream).add(ChannelTypes.WEB)
-
-    can_download = preferences['account_dataset_download'] == 'on' or preferences['account_dataset_download'] or preferences['account_dataset_download'] == 'True'
-    can_export = True
-    can_share = False
-
-    # esto no debería estar????? :) dpadula y la c.... de tu....
-    #DataStreamDBDAO().hit(id, ChannelTypes.WEB)
-    notes = datastream['notes']
-
-    return render_to_response('viewDataStream/index.html', locals())
 
 
 def action_flexmonster(request, id, slug):
