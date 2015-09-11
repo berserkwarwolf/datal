88b08ff8fc8198729faf2a758b1c7f5179e746f5
Gaston Avila <avila.gas@gmail.com>
2015-09-11 15:52:00 -0300

    agregar url para preview de seleccion de datos de mapa

--- workspace/manageVisualizations/urls.py
+++ workspace/manageVisualizations/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     url(r'^filter_datastream$', filterDatastreams, name='manageDatastream.filter'),
     url(r'^create$', create, name='manageVisualizations.create'),
     url(r'^preview$', preview, name='manageVisualizations.preview'),
+    url(r'^preview/(?P<datastream_revision_id>\d+)/map$', preview_map, name='manageVisualizations.preview_map'),
     url(r'^(?P<revision_id>\d+)$', action_view, name='manageVisualizations.view'),
     url(r'^remove/(?P<type>[a-z]+)/(?P<visualization_revision_id>\d+)$', remove, name='manageVisualizations.remove'),
     url(r'^remove/(?P<visualization_revision_id>\d+)$', remove, name='manageVisualizations.remove'),
