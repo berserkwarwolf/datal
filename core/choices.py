d0d24b40316e62d91bda391e20abb2e5bd7f2939
avdata99 <andres@data99.com.ar>
2015-09-16 11:34:23 -0300

    chioces to boolean

--- core/choices.py
+++ core/choices.py
@@ -228,24 +228,29 @@ THRESHOLD_NAME_CHOICES = (
 )
 
 ACCOUNT_PREFERENCES_AVAILABLE_KEYS = (
-     ('ds.detail.full.css', 'ds.detail.full.css')
-    ,('ds.detail.full.javascript', 'ds.detail.full.javascript')
+    #STRING codigo CSS ---------------------------
+     ('ds.detail.full.css', 'ds.detail.full.css') 
     ,('ds.embed.full.css', 'ds.embed.full.css')
-    ,('ds.embed.full.javascript', 'ds.embed.full.javascript')
     ,('chart.detail.full.css', 'chart.detail.full.css')
-    ,('chart.detail.full.javascript', 'chart.detail.full.javascript')
     ,('chart.embed.full.css', 'chart.embed.full.css')
-    ,('chart.embed.full.javascript', 'chart.embed.full.javascript')
     ,('db.detail.full.css', 'db.detail.full.css')
-    ,('db.detail.full.javascript', 'db.detail.full.javascript')
     ,('search.full.css', 'search.full.css')
-    ,('search.full.javascript', 'search.full.javascript')
     ,('home.full.css', 'home.full.css')
-    ,('home.full.javascript', 'home.full.javascript')
     ,('developers.full.css', 'developers.full.css')
+
+    # STRING codigo JS ---------------------------
+    ,('ds.detail.full.javascript', 'ds.detail.full.javascript') 
+    ,('ds.embed.full.javascript', 'ds.embed.full.javascript')
+    ,('chart.detail.full.javascript', 'chart.detail.full.javascript')
+    ,('chart.embed.full.javascript', 'chart.embed.full.javascript')
+    ,('db.detail.full.javascript', 'db.detail.full.javascript')
+    ,('search.full.javascript', 'search.full.javascript')
+    ,('home.full.javascript', 'home.full.javascript')
     ,('developers.full.javascript', 'developers.full.javascript')
-    ,('account.name', 'account.name')
-    ,('account.bio', 'account.bio')
+
+    
+    ,('account.name', 'account.name') 
+    ,('account.bio', 'account.bio') 
     ,('account.link', 'account.link')
     ,('account.logo', 'account.logo')
     ,('account.url', 'account.url')
@@ -274,7 +279,7 @@ ACCOUNT_PREFERENCES_AVAILABLE_KEYS = (
     ,('enable.junar.footer', 'enable.junar.footer')
     ,('account.featured.dashboards', 'account.featured.dashboards')
     ,('account.enable.sharing', 'account.enable.sharing')
-    ,('account.enable.notes', 'account.enable.notes')
+    ,('account.enable.notes', 'account.enable.notes') # BOOLEAN
     ,('account.title.color', 'account.title.color')
     ,('account.button.bg.color', 'account.button.bg.color')
     ,('account.button.border.color', 'account.button.border.color')
