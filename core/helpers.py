2b077cc6f7b613751a304f8c4cfe5a8def96c13c
Daniel Padula <daniel.padula@gmail.com>
2015-09-17 11:14:50 -0300

    Formato de fecha

--- core/helpers.py
+++ core/helpers.py
@@ -10,8 +10,8 @@ from django.db.models.base import ModelState
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
-       if hasattr(obj, 'isoformat'):
-           return obj.isoformat()
+       if hasattr(obj, 'strftime'):
+           return obj.strftime('%B %d, %Y, %I:%M %p')
        elif isinstance(obj, decimal.Decimal):
            return float(obj)
        elif isinstance(obj, ModelState):
