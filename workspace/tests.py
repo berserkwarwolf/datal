95d58163a301e3d6dcb5adbd4faf69500d6d1916
Daniel Padula <daniel.padula@gmail.com>
2015-09-24 16:54:04 -0300

    Tests de datasets en editor

--- workspace/tests.py
+++ workspace/tests.py
@@ -1,14 +1,21 @@
 from django.test import LiveServerTestCase
+from django.db.models import Max
+from django.core.urlresolvers import reverse
 
+from os.path import expanduser
 from pyvirtualdisplay import Display
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.webdriver import WebDriver
+
+from core.models import Dataset
 
 
 class DATALSeleniumTests(LiveServerTestCase):
     display = None
     driver = None
     workspace_url = 'http://workspace.dev'  # Deberiamos traerlo de settings
+    example_xls_file = '{}{}'.format(expanduser("~"), '/app/docs/tests/Ejemplo.xlsx')
+    example_email = 'test@test.com'
 
     @classmethod
     def setUpClass(cls):
@@ -52,10 +59,21 @@ class DATALSeleniumTests(LiveServerTestCase):
     def logout(self):
         self.driver.get(self.workspace_url + '/signout/')
 
+    def create_dataset(self):
+        self.driver.find_element_by_id('id_datasets_option').click()
+        self.driver.find_element_by_link_text('Nuevo conjunto de datos').click()
+        self.driver.find_element_by_link_text('Desde un archivo...').click()
+        self.driver.find_element_by_id('id_file_data').send_keys(self.example_xls_file)
+        self.driver.find_element_by_class_name('nextButton').click()
+        self.driver.find_element_by_id('id_title').send_keys('Dataset de prueba 1')
+        self.driver.find_element_by_id('id_description').send_keys('Descripcion de prueba 1')
+        self.driver.find_element_by_class_name('saveButton').click()
+
     def test_login_logout(self):
         """
         [Workspace] Test del inicio y cerrado de sesion
         """
+        self.logout()
         self.login_as_admin()
         self.assertEqual(self.driver.current_url, u'http://workspace.dev/welcome/')
         self.logout()
@@ -65,10 +83,9 @@ class DATALSeleniumTests(LiveServerTestCase):
         """
         [Workspace] Test de creacion de dataset tipo File con usuario editor
         """
+        self.logout()
         self.login_as_editor()
-        self.driver.find_element_by_id('id_datasets_option').click()
-        self.driver.find_element_by_link_text('Nuevo conjunto de datos').click()
-        self.driver.find_element_by_link_text('Desde un archivo...').click()
+        self.create_dataset()
         self.driver.save_screenshot('datal.png')
         self.logout()
 
