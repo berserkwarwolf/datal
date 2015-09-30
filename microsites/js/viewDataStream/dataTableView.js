f0197a9e61bfcd26d2862ded2e08f91fa6ba7e41
avdata99 <andres@data99.com.ar>
2015-09-30 17:44:07 -0300

    fix id for flexigrid

--- microsites/js/viewDataStream/dataTableView.js
+++ microsites/js/viewDataStream/dataTableView.js
@@ -314,7 +314,7 @@ var dataTableView = Backbone.View.extend({
 
 		// Init Flexigrid
 		$('.dataTable .data .result').flexigrid({
-			url: '/rest/datastreams/' + dataStream.last_published_revision_id + '/data.grid',
+			url: '/rest/datastreams/' + dataStream.id + '/data.grid',
 			dataType: 'json',
 			colModel: colModel,
 			searchitems : searchArray,
