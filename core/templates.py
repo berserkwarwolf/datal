99f70e0126ffdf27c8ed4ddf4f0d1f19f08323fb
avdata99 <andres@data99.com.ar>
2015-10-30 18:16:35 -0300

    add default json from dict

--- core/templates.py
+++ core/templates.py
@@ -1,6 +1,9 @@
 from django.template import Context, Template
 import json
 import datetime
+import logging 
+logger = logging.getLogger(__name__)
+
 
 class DataStreamOutputBigDataTemplate(Template):
 
@@ -96,4 +99,29 @@ class DefaultAnswer(Template):
         context = {"data": data}
         ctx = Context(context)
         return super(DefaultAnswer, self).render(ctx)
+
+class DefaultDictToJson(Template):
+    """ 
+    entregar un diccionario como Json como respuesta. 
+    No usa realmente la clase <Template>
+    No se requiere un template, se deslpiega el contenido del 
+        diccionario (o una version normalizada {status: true|false, data: CONTENIDO})
+    """
     
+    def render(self, data, normalize=False):
+        """ render dictionario. Normalize es para enviar status y data con el contenido"""
+        
+        if normalize:
+            context = {"status": True, "data": data}
+        else:
+            context = data
+        
+        try:
+            response = json.dumps(context)
+        except Exception, e:
+            error = "Invalida JSON parse error: %s " % str(e)
+            logger.error(error)
+            data = {"status": False, "messages": [error]}
+            response = DefaultAnswer().render(status=False, messages=[error])
+
+        return response
