7ed3e96f7107c410b4a4e40424059594ad14881c
Juan Pablo <juan33650@outlook.com>
2015-11-04 16:11:08 -0300

    [Workspace] Modelo y vista Backbone para creación de conjunto de datos a partir de calendario Office 365

--- workspace/media/workspace/scripts/processManager/dataset/collectOffice365Model.js
+++ workspace/media/workspace/scripts/processManager/dataset/collectOffice365Model.js
@@ -1,5 +1,21 @@
-var CollectOffice365eModel = StepModel.extend({
+var CollectOffice365Model = StepModel.extend({
 	
-	
+	defaults:{
+		calendar: "",
+		range: "",
+		collect_type: 3
+	},
+
+	setOutput: function(){
+
+		var output = this.get('output');
+
+		output.calendar = $.trim( this.get('calendar') );
+		output.range = $.trim( this.get('range') );
+		output.collect_type = this.get('collect_type');
+		
+		this.set('output',output);
+
+	}
 
 });
--- workspace/media/workspace/scripts/processManager/dataset/collectOffice365View.js
+++ workspace/media/workspace/scripts/processManager/dataset/collectOffice365View.js
@@ -1,5 +1,51 @@
 var CollectOffice365View = StepView.extend({
 
-	
+	name: null,
+	template: null,
+	templateHTML: null,
+
+	initialize: function(){
+
+		this.templateHTML = $("#id_collectOffice365Template").html();
+		this.template = _.template( this.templateHTML );
+		this.name = this.model.get('name');
+
+		// Overriding el and $el
+		this.el = '.step[data-step='+this.name+']',
+		this.$el = $(this.el);
+
+		// Right way to extend events without overriding the parent ones
+		var eventsObject = {}
+		eventsObject['click .step[data-step='+this.name+'] .navigation .backButton'] = 'onPreviousButtonClicked';
+		eventsObject['click .step[data-step='+this.name+'] .navigation .nextButton'] = 'onNextButtonClicked';
+		this.addEvents(eventsObject);
+
+		this.render();
+
+	},
+
+	render: function(){
+
+		var self = this;
+
+		this.$el.find('.formContent').html( this.template( this.model.toJSON() ) );	
+
+		return this;
+	},
+
+	onPreviousButtonClicked: function(){
+		this.previous();
+	},
+
+	onNextButtonClicked: function(){
+
+		var calendar = $.trim( this.model.get('calendar') );
+		this.model.set('calendar', calendar);
+
+		if(this.model.isValid(true)){
+			this.model.setOutput();
+			this.next();
+		}
+	},
 
 });
