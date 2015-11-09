7959967120be9c607467ec07f7401adc51ce6c37
Daniel Padula <daniel.padula@gmail.com>
2015-11-09 18:50:02 -0300

    No mas imagen de portada aca

--- salt/roots/salt/core/tomcat-env.sh
+++ salt/roots/salt/core/tomcat-env.sh
@@ -1,1 +1,1 @@
-export CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -XX:PermSize=64m -XX:MaxPermSize=256m"
+export CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m"
