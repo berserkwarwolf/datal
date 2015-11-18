af072f2a3459ed030b16c1c01d7a468f9fe6675d
Matias Saguir <mativs@gmail.com>
2015-11-18 11:13:07 -0300

    Maximo 55 caracteres en title the theme 4

--- workspace/media/workspace/scripts/personalizeHome/theme4Model.js
+++ workspace/media/workspace/scripts/personalizeHome/theme4Model.js
@@ -15,8 +15,8 @@ var theme4Model = Backbone.Model.extend({
                 required: true,
                 msg: gettext('VALIDATE-REQUIREDFIELD-TEXT')
             },{
-                maxLength: 30,
-                msg: gettext('VALIDATE-MAXLENGTH-TEXT-1') + ' 30 ' + gettext('VALIDATE-MAXLENGTH-TEXT-2')
+                maxLength: 55,
+                msg: gettext('VALIDATE-MAXLENGTH-TEXT-1') + ' 55 ' + gettext('VALIDATE-MAXLENGTH-TEXT-2')
             }
         ]
     },
