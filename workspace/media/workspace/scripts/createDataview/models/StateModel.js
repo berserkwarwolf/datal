6c63e713a9764dbf0fc9cabf0215f9cfdae194e7
Gaston Avila <avila.gas@gmail.com>
2015-11-23 16:34:38 -0300

    prevenir estados con step < 0

--- workspace/media/workspace/scripts/createDataview/models/StateModel.js
+++ workspace/media/workspace/scripts/createDataview/models/StateModel.js
@@ -2,10 +2,13 @@ var StateModel = Backbone.Model.extend({
     last: 4,
 
     next: function () {
-       this.set('step', this.get('step') + 1);
+        this.set('step', this.get('step') + 1);
     },
 
     prev: function () {
-       this.set('step', this.get('step') - 1);
+        var step = this.get('step');
+        if (step > 0) {
+            this.set('step', step - 1);
+        }
     } 
 });
