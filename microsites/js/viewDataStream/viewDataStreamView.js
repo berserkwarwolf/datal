6cc7e1945cdba88c94252107e441bad481e8cdfb
Ignacio Sans <ignacio@ideaslocomotoras.com.ar>
2015-12-05 21:17:00 -0300

    [microsites] ajustes a viewDataStream para soportar plugin

--- microsites/js/viewDataStream/viewDataStreamView.js
+++ microsites/js/viewDataStream/viewDataStreamView.js
@@ -60,7 +60,7 @@ _.extend(viewDataStreamView.prototype, Backbone.View.prototype, {
 		this.preloadImages();
 
 		//this.model contains all the necessary data
-		this.theDataTable = new dataTableView({model: new dataTable(), dataStream: this.model});
+		this.theDataTable = new dataTableView({model: new dataTable(), dataStream: this.model, parentView: this });
 		
 		//if data changes, then redraw
 		this.listenTo(this.model, "change", this.render);
@@ -222,32 +222,32 @@ _.extend(viewDataStreamView.prototype, Backbone.View.prototype, {
 
 	permalinkHelper: function(){
 
-		var permalink = this.model.attributes.permaLink,
-			self = this;
+		// var permalink = this.model.attributes.permaLink,
+		// 	self = this;
 
-		$('#id_permalink').val(permalink);
+		// $('#id_permalink').val(permalink);
 
-		if (typeof(BitlyClient) != 'undefined') {
-      BitlyClient.shorten( permalink, function(pData){
+		// if (typeof(BitlyClient) != 'undefined') {
+  //     BitlyClient.shorten( permalink, function(pData){
 
-				var response,
-				  shortUrl;
+		// 		var response,
+		// 		  shortUrl;
 				
-				for(var r in pData.results) {
-		    	response = pData.results[r];
-		  		break;
-				}
+		// 		for(var r in pData.results) {
+		//     	response = pData.results[r];
+		//   		break;
+		// 		}
 
-				shortUrl = response['shortUrl'];
+		// 		shortUrl = response['shortUrl'];
 
-		    if(shortUrl){
-		      self.model.attributes.shortUrl = shortUrl;
-		      $('#id_permalink').val(shortUrl);
-		    }
+		//     if(shortUrl){
+		//       self.model.attributes.shortUrl = shortUrl;
+		//       $('#id_permalink').val(shortUrl);
+		//     }
 
-			});
+		// 	});
 
-    }
+  //   }
 
 	},
 
