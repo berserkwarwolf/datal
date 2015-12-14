41a9716d5e6904e4657b3d3485ae2eb533bd6793
Daniel Padula <daniel.padula@gmail.com>
2015-12-14 11:36:33 -0300

    Nuevas settings

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,15 +1,19 @@
 from django.conf import settings
 
+
 def request_context(request):
     d = {'auth_manager': request.auth_manager}
     
-    my_settings = {'BASE_URI' : settings.BASE_URI,
-                  'API_URI' : settings.API_URI,
-                  'API_KEY' : settings.API_KEY,
-                  'VERSION_JS_CSS': settings.VERSION_JS_CSS,
-                  'WORKSPACE_URI': settings.WORKSPACE_URI,
-                  'DOMAINS': settings.DOMAINS,
-                  'DOC_API_URL': settings.DOC_API_URL}
+    my_settings = {
+        'BASE_URI' : settings.BASE_URI,
+        'API_URI' : settings.API_URI,
+        'API_KEY' : settings.API_KEY,
+        'VERSION_JS_CSS': settings.VERSION_JS_CSS,
+        'WORKSPACE_URI': settings.WORKSPACE_URI,
+        'DOMAINS': settings.DOMAINS,
+        'DOC_API_URL': settings.DOC_API_URL,
+        'APPLICATION_DETAILS': settings.APPLICATION_DETAILS,
+    }
                   
     d['settings'] = my_settings
     d['preference'] = request.preferences
