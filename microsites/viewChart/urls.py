7bea7797aad9d6399f104b77f4b6f169b5504498
avdata99 <andres@data99.com.ar>
2015-12-21 15:48:03 -0300

    add embed for viz

--- microsites/viewChart/urls.py
+++ microsites/viewChart/urls.py
@@ -4,4 +4,5 @@ from microsites.viewChart.views import *
 urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/(?P<slug>[A-Za-z0-9\-]+)?$', 'microsites.viewChart.views.view', name='chart_manager.view'),
     url(r'^(?P<id>\d+)-(?P<slug>[A-Za-z0-9\-]+).download$', 'microsites.viewDataStream.views.download', name='viewChart.download'),
+    url(r'^embed/(?P<guid>[A-Z0-9\-]+)$', 'microsites.viewChart.views.embed', name='viewChart.embed'),
 )
