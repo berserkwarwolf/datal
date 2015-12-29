82e88c23f24ea16acad2c5b7fbee8d7a0ba0a9be
Matias Saguir <mativs@gmail.com>
2015-12-29 20:22:03 -0300

    Post migrate cleaan impl_details selection

--- core/management/commands/post_migrate.py
+++ core/management/commands/post_migrate.py
@@ -2,8 +2,9 @@ from django.core.management.base import BaseCommand
 
 from optparse import make_option
 
-from core.models import User, Grant
+from core.models import User, Grant, VisualizationRevision
 
+import json
 
 class Command(BaseCommand):
 
@@ -12,3 +13,17 @@ class Command(BaseCommand):
 
         # TODO: Debemos buscar los usuarios con Roles que no usamos mas y cambiarlos por los nuevos
         # TODO
+        for rev in VisualizationRevision.objects.all():
+            imp = json.loads(rev.impl_details)
+            if 'labelSelection' in imp:
+                imp['labelSelection'] = imp['labelSelection'].replace(' ', '')
+            if 'latitudSelection' in imp:
+                imp['latitudSelection'] = imp['latitudSelection'].replace(' ', '')
+            if 'headerSelection' in imp:
+                imp['headerSelection']  = imp['headerSelection'] .replace(' ', '') 
+            if 'longitudSelection' in imp:
+                imp['longitudSelection'] = imp['longitudSelection'].replace(' ', '')
+            if 'traceSelection' in imp:
+                imp['traceSelection'] = imp['traceSelection'].replace(' ', '')
+            rev.impl_details = json.dumps(imp)
+            rev.save()
