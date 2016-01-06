f573086a66897f0428ca943d9b5a70fff5574ad4
ignacionf <ignacionf@gmail.com>
2016-01-06 15:35:19 -0300

    se hace lo que se puede con estos métodos sobre escritos

--- core/shortcuts.py
+++ core/shortcuts.py
@@ -4,6 +4,9 @@ from django.template import RequestContext
 
 
 def render_to_response(template, dictionary, mimetype=settings.DEFAULT_CONTENT_TYPE):
-    request = dictionary.pop('request')
+    try:
+        request = dictionary.pop('request')
+    except:
+        pass #dale que va
     context_instance = RequestContext(request)
     return django_render_to_response(template, dictionary, context_instance, mimetype=mimetype)
